8f3b35aa78f7b0f23303041c10b763823fd0931d
cgens67 <cgenshin587@gmail.com>
2026-05-03 16:17:40 +0800

    Create AppLanguage.kt

---
+++ app/src/main/kotlin/com/github/musicyou/enums/AppLanguage.kt
@@ -0,0 +1,10 @@
+package com.github.musicyou.enums
+
+enum class AppLanguage(val code: String, val displayName: String) {
+    SYSTEM("", "System Default"),
+    EN("en", "English"),
+    ES("es", "Español"),
+    DE("de", "Deutsch"),
+    FR("fr", "Français"),
+    IT("it", "Italiano")
+}
