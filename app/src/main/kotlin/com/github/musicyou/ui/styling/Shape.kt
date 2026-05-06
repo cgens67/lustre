fe355c1eb06bf32505300df1b6919361f43ed9d2
cgens67 <cgenshin587@gmail.com>
2026-05-06 23:46:25 +0800

    Create Shape.kt

---
+++ app/src/main/kotlin/com/github/musicyou/ui/styling/Shape.kt
@@ -0,0 +1,13 @@
+package com.github.musicyou.ui.styling
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val ExpressiveShapes = Shapes(
+    extraSmall = RoundedCornerShape(8.dp),
+    small = RoundedCornerShape(16.dp),
+    medium = RoundedCornerShape(24.dp),
+    large = RoundedCornerShape(32.dp),
+    extraLarge = RoundedCornerShape(48.dp)
+)
